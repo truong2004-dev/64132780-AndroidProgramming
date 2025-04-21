c9ce1796ec44a3216e5bb2fbef309f3c6f4cb2a3
truong2004-dev <truongnguyenminh382@gmail.com>
2025-04-21 08:11:08 +0700

    first commit

---
+++ Fragments_Dynamically/app/src/main/java/truong/edu/fragments_dynamically/MainActivity.java
@@ -0,0 +1,29 @@
+package truong.edu.fragments_dynamically;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.frameContentHolder, new ContentFragment()).commit();
+        fragmentManager.beginTransaction().add(R.id.frameFooterHolder, new FooterFragment()).commit();
+
+    }
+}
