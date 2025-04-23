f03f73340bb3b58e3c80f3ed851ec7d4fc05873e
truong2004-dev <truongnguyenminh382@gmail.com>
2025-04-23 10:34:15 +0700

    first commit

---
+++ BottomNavigationbar/app/src/main/java/truong/edu/bottomnavigationbar/MainActivity.java
@@ -0,0 +1,40 @@
+package truong.edu.bottomnavigationbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonID = item.getItemId();
+                if (mnuItemDuocChonID ==R.id.mnu_home) {
+                    Toast.makeText(MainActivity.this, "thay HOME", Toast.LENGTH_SHORT).show();
+                }else if (mnuItemDuocChonID ==R.id.mnu_search) {
+                    Toast.makeText(MainActivity.this, "thay SEARCH", Toast.LENGTH_SHORT).show();
+                }else if (mnuItemDuocChonID ==R.id.mnu_profile) {
+                    Toast.makeText(MainActivity.this, "thay PROFILE", Toast.LENGTH_SHORT).show();
+                }else return false;
+                return true;
+            }
+        });
+    }
+}
